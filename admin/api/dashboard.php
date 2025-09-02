8c9d2f6b3736873a4cd10c18dcc626af26719288
kavishka07 <tvkrp325@gmail.com>
2025-09-02 18:39:42 +0530

    user management new update

--- admin/api/dashboard.php
+++ admin/api/dashboard.php
@@ -4,11 +4,12 @@ require_once(__DIR__ . '/../models/admin.php');
 require_once(__DIR__ . '/../../config/cors.php');
 
 $admin = new Admin();
-// Fetch counts from admin model (should return an associative array)
-$counts = $admin->getCounts();
+$data = $admin->getCounts();
 
 echo json_encode([
     "success" => true,
-    "data" => $counts
+    "data" => $data
 ]);
+
+exit;
 
--- admin/api/users.php
+++ admin/api/users.php
@@ -5,13 +5,11 @@ require_once(__DIR__ . '/../../config/cors.php');
 
 $admin = new Admin();
 $users = $admin->getNonAdminUsers();
-
 echo json_encode([
     "success" => true,
-<<<<<<< HEAD
     "data" => $users
 ]);
-=======
-    "data" => $list
-]);
->>>>>>> 0c8c34f472b4681f5c8e8ad3692b7c42492c26a0
+
+exit;
+
+
